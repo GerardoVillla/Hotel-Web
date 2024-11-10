1424f53d1a879f78c145cfefa090173952797f7c
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-11-09 18:24:59 -0600

    chore(servidor): cambia el orden de presentacion del proyecto

---
+++ src/servidor/config/conexiondb.php
@@ -0,0 +1,22 @@
+<?php
+class Conexiondb{
+	//Configuracion por defecto que trae xampp 
+	private $server = "localhost";
+	private $user = "root";
+	private $pass = "";
+	//Nombre que le se puso a la bd en phpmyadmin
+	private $db = "hotelecologico";
+	public $conexion;
+
+	public function _getConnection(){
+		$this->conexion = new mysqli($this->server, $this->user, $this->pass, $this->db);
+		if($this->conexion->connect_error){
+			die("Conexion fallida: " . $this->conexion->connect_error);
+		}else{
+			echo"conectado";
+		}
+		return $this->conexion;
+	}
+
+
+}
