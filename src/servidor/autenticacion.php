d10eff0787fc28a3e7cce637fd75404cb7e0e21c
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-11-13 00:41:48 -0600

    refactor(servidor): refactoriza autenticacion de usuarios

--- src/servidor/autenticacion.php
+++ src/servidor/autenticacion.php
@@ -6,13 +6,11 @@ require_once ("sesion.php");
 
 if(isset($_POST["action"])){
 	$peticionRecibida = $_POST["action"];
+	$usuario = $_POST["txt_nombre"];
+	$contrasena = $_POST["pass_contraseña"];
 	if($peticionRecibida == "iniciarSesion"){
-		$usuario = $_POST["txt_nombre"];
-		$contrasena = $_POST["pass_contraseña"];
 		iniciarSesion($usuario,$contrasena);
 	}else if($peticionRecibida == "registrarUsuario"){
-		$usuario = $_POST["txt_nombre"];
-		$contrasena = $_POST["pass_contraseña"];
 		registrarUsuario($usuario,$contrasena);
 	}
 }
