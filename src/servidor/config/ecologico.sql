031b7c1a053aa44ba89bed4859081836dcf50233
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-11-17 14:12:09 -0600

    chore(server): agrega cantidad de habitaciones en la BD

--- src/servidor/config/ecologico.sql
+++ src/servidor/config/ecologico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-11-2024 a las 03:11:26
+-- Tiempo de generación: 17-11-2024 a las 21:10:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,15 +44,15 @@ CREATE TABLE `habitaciones` (
 --
 
 INSERT INTO `habitaciones` (`idhabitacion`, `nombre`, `categoria`, `disponibles`, `numHabitaciones`, `descripcion`, `costoPorNoche`, `capacidadDePersonas`, `urlImagen`) VALUES
-(1, 'Cascada Secreta', 'Deluxe', 1, 0, 'Habitación con ambiente de cascada, incluye sonidos relajantes de agua.', 2150, 3, 'cascada-secreta.jpg'),
-(2, 'Ártico Blanco', 'Suite', 1, 0, 'Suite ambientada en el Ártico, con efectos de iluminación y una decoración minimalista y fresca.', 3900, 4, 'artico-blanco.jpg'),
-(3, 'Santuario Marino', 'Estandar', 1, 0, 'Habitación con una decoración marina, con elementos de madera sostenible y vista a un estanque.', 1000, 1, 'santuario-marino.jpg'),
-(4, 'Nido Tropical', 'Deluxe', 1, 0, 'Habitación de lujo con temática tropical, incluye jardín interior y plantas exóticas.', 1900, 2, 'nido-tropical.jpg'),
-(5, 'Fuego Vivo', 'Suite', 1, 0, 'Suite inspirada en un volcán, con tonos cálidos y decoración con rocas naturales.', 4200, 4, 'fuego-vivo.jpg'),
-(6, 'Desierto Vivo', 'Deluxe', 1, 0, 'Habitación con decoración inspirada en el desierto, con colores cálidos y plantas del clima árido.', 2000, 3, 'desierto-vivo.jpg'),
-(7, 'Selva Mística', 'Suite', 1, 0, 'Suite con inspiración amazónica, amplio espacio verde y decoración temática de jungla.', 3500, 4, 'selva-mistica.jpg'),
-(8, 'Refugio Verde', 'Estandar', 1, 0, 'Habitación con vista al jardín, decorada con materiales reciclados y productos ecológicos.', 1200, 2, 'refugio-verde.jpg'),
-(9, 'Bambú Natural', 'Estandar', 1, 0, 'Habitación inspirada en el bambú, con un estilo rústico y sostenible.', 1300, 2, 'bambu-natural.jpg');
+(1, 'Cascada Secreta', 'Deluxe', 1, 10, 'Habitación con ambiente de cascada, incluye sonidos relajantes de agua.', 2150, 3, 'cascada-secreta.jpg'),
+(2, 'Ártico Blanco', 'Suite', 1, 8, 'Suite ambientada en el Ártico, con efectos de iluminación y una decoración minimalista y fresca.', 3900, 4, 'artico-blanco.jpg'),
+(3, 'Santuario Marino', 'Estandar', 1, 4, 'Habitación con una decoración marina, con elementos de madera sostenible y vista a un estanque.', 1000, 1, 'santuario-marino.jpg'),
+(4, 'Nido Tropical', 'Deluxe', 1, 5, 'Habitación de lujo con temática tropical, incluye jardín interior y plantas exóticas.', 1900, 2, 'nido-tropical.jpg'),
+(5, 'Fuego Vivo', 'Suite', 1, 5, 'Suite inspirada en un volcán, con tonos cálidos y decoración con rocas naturales.', 4200, 4, 'fuego-vivo.jpg'),
+(6, 'Desierto Vivo', 'Deluxe', 1, 3, 'Habitación con decoración inspirada en el desierto, con colores cálidos y plantas del clima árido.', 2000, 3, 'desierto-vivo.jpg'),
+(7, 'Selva Mística', 'Suite', 1, 8, 'Suite con inspiración amazónica, amplio espacio verde y decoración temática de jungla.', 3500, 4, 'selva-mistica.jpg'),
+(8, 'Refugio Verde', 'Estandar', 1, 2, 'Habitación con vista al jardín, decorada con materiales reciclados y productos ecológicos.', 1200, 2, 'refugio-verde.jpg'),
+(9, 'Bambú Natural', 'Estandar', 1, 5, 'Habitación inspirada en el bambú, con un estilo rústico y sostenible.', 1300, 2, 'bambu-natural.jpg');
 
 --
 -- Índices para tablas volcadas
@@ -73,7 +73,7 @@ ALTER TABLE `habitaciones`
 -- AUTO_INCREMENT de la tabla `habitaciones`
 --
 ALTER TABLE `habitaciones`
-  MODIFY `idhabitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `idhabitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
