a8c33737e37769117e8ddcf6849c6287d6cd7a97
GeraVillla <A22216369@alumnos.uady.mx>
2024-11-22 20:40:30 -0600

    chore(login): agrega titulo a la vista principal

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/ecologico/src/cliente/css/iniciosesion-styles.css">
-    <title>Document</title>
+    <title>Hotel Ecologico</title>
 </head>
 <body>
     <div id="imagen-seccion">
