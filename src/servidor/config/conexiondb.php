4fb9265f96e0fb84c24ba09f15667ffab050826e
GeraVillla <A22216369@alumnos.uady.mx>
2024-11-22 20:52:58 -0600

    chore(sesion.php): actualiza a $GLOBALS los atributos de la db

--- src/servidor/config/conexiondb.php
+++ src/servidor/config/conexiondb.php
@@ -1,19 +1,31 @@
 <?php
+
+$GLOBALS["servidor"] = "localhost";
+$GLOBALS["usuario"] = "root";
+$GLOBALS["contrasena"] = "";
+$GLOBALS["base_datos"] = "ecologico";
+$GLOBALS["raiz_sitio"] = "http://localhost/ecologico/";
 class Conexiondb{
 	//Configuracion por defecto que trae xampp 
-	private $server = "localhost";
-	private $user = "root";
-	private $pass = "";
+
+	private $servidor;
+	private $usuario;
+	private $contrasena;
 	//Nombre que le se puso a la bd en phpmyadmin
-	private $db = "hotelecologico";
+	private $db;
 	public $conexion;
 
+	public function __construct(){
+		$this->servidor = $GLOBALS["servidor"];
+		$this->usuario = $GLOBALS["servidor"];
+		$this->contrasena = $GLOBALS["servidor"];
+		$this->db = $GLOBALS["servidor"];
+	}
+
 	public function getConnection(){
-		$this->conexion = new mysqli($this->server, $this->user, $this->pass, $this->db);
+		$this->conexion = new mysqli($this->servidor, $this->user, $this->pass, $this->db);
 		if($this->conexion->connect_error){
 			die("Conexion fallida: " . $this->conexion->connect_error);
-		}else{
-			//echo"conectado";
 		}
 		return $this->conexion;
 	}
