94a5aa6c6cbdcbcdb0fb76564b9e7c2cb22a9abb
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-11-25 23:31:49 -0600

    feat(principal): agrega iconos al header

--- src/cliente/view/principal.php
+++ src/cliente/view/principal.php
@@ -13,14 +13,14 @@ validarSesionPantallaPrincipal();
 
 <body>
 	<header>
-		<h1>Hotel ecológico</h1>
+		<h1><img id="logo" src="../recursos/img/iconos/logopng.png" alt="">Ek' Balam</h1>
 		<nav>
 			<ul>
-				<li id="cerrarSesion"><a href="#">Cerrar sesion</a></li>
-				<li><a href="pagar.php">Carrito</a></li>
-				<li><a href="misReservaciones.php">Ver mis reservaciones</a></li>
-				<li><a href="#nosotros-tarjeta">Nosotros</a></li>
-				<li><a href="buscar.php">Buscar</a></li>
+				<li><a href="pagar.php"><img class="icono-encabezado" src="../recursos/img/iconos/carrito-de-compras.png"></a></li>
+				<li><a href="buscar.php"><img class="icono-encabezado" src="../recursos/img/iconos/lupa.png"></a></li>
+				<li><a href="misReservaciones.php"><img class="icono-encabezado" src="../recursos/img/iconos/avatar.png"></a></li>
+				<li><a href="#nosotros-tarjeta"><img class="icono-encabezado" src="../recursos/img/iconos/informacion.png" alt=""></a></li>
+				<li id="cerrarSesion"><a href="#"><img class="icono-encabezado" src="../recursos/img/iconos/cerrar-sesion.png"></a></li>
 				<li id="btn-reservar"><a href="reservar.php">Reservar</a></li>
 			</ul>
 		</nav>
@@ -28,15 +28,12 @@ validarSesionPantallaPrincipal();
 	<!-- Contenido de la pagina -->
 	<div class="contenido">
 		<div id="img-home">
-			<img src="../recursos/img/principal/home.png" alt="img de bienvenida">
+			<img src="../recursos/img/principal/principal2.jpg" alt="img de bienvenida">
 		</div>
-
-
-
 		<section id="bienvenida-tarjeta">
 			<div class="articulo">
-				<h1>Bienvenido a <span>Hotel Ek' Balam</span></h1>
-				<p>Localizado a tan solo 3 minutos del Aeropuerto Internacional de Mérida y a 10 minutos del centro
+				<h1 class="playfair-display-subtitulo">Bienvenido a <span>Hotel Ek' Balam</span></h1>
+				<p class="playfair-display-parrafo">Localizado a tan solo 3 minutos del Aeropuerto Internacional de Mérida y a 10 minutos del centro
 					histórico.
 					El Hotel Hacienda Inn ofrece una opción única en Yucatán.
 					Se caracteriza por una gran personalidad, con un ambiente cómodo y relajado con un estilo
