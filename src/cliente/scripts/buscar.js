85b903f92cd6025372359dc9dd5002f1bb3eb72e
JoseMurc <A22216366@alumnos.uady.mx>
2024-11-28 16:46:38 -0600

    cambios

--- src/cliente/scripts/buscar.js
+++ src/cliente/scripts/buscar.js
@@ -23,12 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     <option value="asc">De menor a mayor</option>
                     <option value="desc">De mayor a menor</option>
                 `;
+                filtroSecundario.style.display = 'block';
                 break;
             case 'alfabetico':
                 filtroSecundario.innerHTML = `
                     <option value="asc">De la A a la Z</option>
                     <option value="desc">De la Z a la A</option>
                 `;
+                filtroSecundario.style.display = 'block';
                 break;
             case 'categoria':
                 filtroSecundario.innerHTML = `
@@ -36,8 +38,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     <option value="Suite">Suite</option>
                     <option value="Deluxe">Deluxe</option>
                 `;
+                filtroSecundario.style.display = 'block';
+                break;
+            default:
+                filtroSecundario.style.display = 'none'; // Ocultar filtro secundario si está en "todo"
                 break;
         }
-        filtroSecundario.style.display = 'block';
+    });
+
+    formBusqueda.addEventListener('submit', (event) => {
+        if (!busquedaEntrada.value.trim()) {
+            event.preventDefault(); // Evitar que se envíe el formulario
+            alert('Por favor, escribe algo en la barra de búsqueda.');
+        }
     });
 });
